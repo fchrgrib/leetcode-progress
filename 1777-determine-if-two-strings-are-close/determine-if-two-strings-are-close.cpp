be8ec2a77584273457436c9c099a203eec97f86a
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-01-18 22:48:39 +0700

    Time: 266 ms (5.00%) | Memory: 50.1 MB (5.11%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -3,17 +3,6 @@ public:
     bool closeStrings(string word1, string word2) {
         if (word1.size() != word2.size()) return false;
 
-        map<char, bool> temp11, temp22;
-        for(int i = 0; i< word1.size();i++){
-            temp11[word1[i]] = true;
-            temp22[word2[i]] = true; 
-        }
-
-        for(int i = 0; i< word1.size();i++){
-            if(!temp11[word2[i]]) return false;
-            if(!temp22[word1[i]]) return false; 
-        }
-
         map<char, int> temp1, temp2;
         priority_queue<int> pq1,pq2;
         for(int i = 0; i< word1.size(); i++){
@@ -21,6 +10,7 @@ public:
             temp2[word2[i]]++;
         }
         for(int i = 0; i< word1.size(); i++){
+            if(!temp1[word2[i]] || !temp2[word1[i]]) return false;
             pq1.push(temp1[word1[i]]);
             pq2.push(temp2[word2[i]]);
         }
