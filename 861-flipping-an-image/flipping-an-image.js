b98b5e9eaa148e2cb7878a063e93c61196c1b7a3
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-02-10 00:16:35 +0700

    Time: 1 ms (66.50%) | Memory: 51.7 MB (72.09%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.js
+++ 861-flipping-an-image/flipping-an-image.js
@@ -5,16 +5,16 @@
 var flipAndInvertImage = function(image) {
     for(let i = 0;i<image.length;i++){
         let l = 0, r = image.length - 1;
-        while(l<r){
+        while(l<=r){
             let tmp1 = image[i][l];
             image[i][l] = image[i][r];
             image[i][r] = tmp1;
-
-            image[i][l] = (image[i][l] === 1)?0:1;
+            
+            if(l!=r) image[i][l] = (image[i][l] === 1)?0:1;
             image[i][r] = (image[i][r] === 1)?0:1;
             l++;r--;
         }
-        if(r-l == 0) image[i][l]= (image[i][l] === 1)?0:1;
+        // if(r-l == 0) image[i][l]= (image[i][l] === 1)?0:1;
     }
 
     return image;
