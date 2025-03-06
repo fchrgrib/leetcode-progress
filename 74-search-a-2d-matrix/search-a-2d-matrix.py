65d0d11e4fad469a2c5881f41204d61d0547e5d8
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-03-07 05:22:23 +0700

    Time: 0 ms (100.00%) | Memory: 18 MB (99.24%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,35 +1,15 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        l = (0, 0)
-        r = (len(matrix)-1, len(matrix[0])-1)
-        if matrix[l[0]][l[1]] == target or matrix[r[0]][r[1]] == target:
-            return True
-        temp = (len(matrix[0])*l[0]+l[1] + len(matrix[0])*r[0]+r[1])//2
-        m = (temp//len(matrix[0]), temp%len(matrix[0]))
-
-        while len(matrix[0])*l[0]+l[1] < len(matrix[0])*r[0]+r[1]:
-            if matrix[m[0]][m[1]] == target:
+        n = len(matrix)
+        m = len(matrix[0])
+        i = 0
+        j = m-1
+        while (i < n  and j >=0):
+            if (matrix[i][j] == target):
                 return True
-            if matrix[l[0]][l[1]] == target or matrix[r[0]][r[1]] == target:
-                return True
-            if matrix[m[0]][m[1]] > target:
-                if m[1]-1<0:
-                    x = m[0]-1
-                    y = len(matrix[0])-1
-                else:
-                    x = m[0]
-                    y = m[1]-1
-                r = (x, y)
-            if matrix[m[0]][m[1]] < target:
-                if m[1]+1>len(matrix[0])-1:
-                    x = m[0]+1
-                    y = 0
-                else:
-                    x = m[0]
-                    y = m[1]+1
-                l = (x, y)
-            temp = (len(matrix[0])*l[0]+l[1] + len(matrix[0])*r[0]+r[1])//2
-            m = (temp//len(matrix[0]), temp%len(matrix[0]))
-
+            if (matrix[i][j] > target):
+                j-=1
+            else:
+                i+=1
         return False
 
