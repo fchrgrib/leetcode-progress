2504ed72f6d06e05ea39168390d298e574528d3a
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-04-22 22:22:29 +0700

    Time: 3 ms (13.04%) | Memory: 17.9 MB (17.00%) - LeetSync

---
+++ 1080-camelcase-matching/camelcase-matching.py
@@ -0,0 +1,26 @@
+class Solution:
+    def camelMatch(self, q: List[str], p: str) -> List[bool]:
+        n = len(q)
+        res = [False for _ in range(n)]
+
+        def is_match(l1, l2):
+            i1 = i2 = 0
+
+            n1 = len(l1)
+            n2 = len(l2)
+
+            while i1 < n1:
+                if i2 < n2 and l1[i1] == l2[i2]:
+                    i2+=1
+                    i1+=1
+                    continue
+                if l1[i1].isupper():
+                    i2 = 0
+                i1+=1
+            
+            return i2 >= n2
+        
+        for i in range(n):
+            res[i] = is_match(q[i], p)
+        return res
+        
