194d89aafe41e3bc2fc2b0e3f100af715ee53662
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-11-15 05:32:53 +0700

    Time: 0 ms (100.00%) | Memory: 56.7 MB (18.34%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,13 +3,12 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    const max_el = Math.max(...strs.map((a) => a.length));
-    let res = "";
-    for(let i = 0;i<max_el;i++){
-        const is_same = strs.every((s) => s[i] === strs[0][i])
-        if (!is_same) break;
-        res+=strs[0][i]
+    let prefix = "";
+
+    for (let i = 0; i < strs[0].length; i++) {
+        if (strs.some(s => s[i] !== strs[0][i])) break;
+        prefix += strs[0][i];
     }
 
-    return res
+    return prefix;
 };
