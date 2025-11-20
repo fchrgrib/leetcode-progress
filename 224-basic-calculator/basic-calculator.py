fa6be32dafe645cd7a7f44d8de339a3d6a64779b
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-11-20 07:49:07 +0700

    Time: 47 ms (67.27%) | Memory: 18.3 MB (99.09%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,52 +1,32 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        s = s.replace(" ","")
-        def sum_number(s: str) -> int:
-            # print(s)
-            
-            sm = tmp = 0
-            is_m = 1
-
-            for i in range(len(s)):
-                if s[i] == "+" or s[i] == "-":
-                    sm += (is_m * tmp)
-                    tmp = 0
-                    if s[i] == "+":
-                        is_m = 1
-                    else:
-                        if i>0 and s[i-1] == "-":
-                            is_m*=-1
-                        else:
-                            is_m = -1
-                    continue
-                if s[i].isspace():
-                    continue
-                tmp = (tmp * 10) + int(s[i])
-                    
-            return sm + (is_m * tmp)
-        
         stack = []
-        i = 0
-        l = len(s)
-        while i<l:
-            if s[i] == "(":
-                stack.append(i)
-            if s[i] == ")":
-                tmp_idx = stack.pop()
-                start = tmp_idx+1
-                end = start+ (i-(start))
-                tmp_res = sum_number(s[start:end])
-                s = s[:tmp_idx] + str(tmp_res) + s[i+1:]
-                # print(s)
-                # print(s)
-                i = len(s[:tmp_idx] + str(tmp_res)) -1
-                # print("index: ",i)
-                l = len(s)
-            i+=1
-        # print(s)
+        result = 0
+        num = 0
+        sign = 1
         
-        return sum_number(s)
-                        
-
-
+        stack.append(sign) 
         
+        for char in s:
+            if char.isdigit():
+                num = num * 10 + int(char)
+                
+            elif char in ('+', '-'):
+                result += num * sign * stack[-1]
+                num = 0
+                sign = 1 if char == '+' else -1
+                
+            elif char == '(':
+                stack.append(sign * stack[-1])
+                sign = 1
+                
+            elif char == ')':
+                result += num * sign * stack[-1]
+                num = 0
+                stack.pop()
+                sign = 1
+                
+        if num != 0:
+            result += num * sign * stack[-1]
+            
+        return result
