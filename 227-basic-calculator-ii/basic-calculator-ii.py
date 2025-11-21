d390282254a18daa4f5c065dde60e7e5ac6b9708
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-11-21 12:04:21 +0700

    Time: 60 ms (73.65%) | Memory: 18.8 MB (98.51%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,65 +1,28 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        sign = 1
-        num = res = 0
-        symb = []
-        md = []
-
-        def md_res(md, symb, num):
-            if len(md) == 0:
-                return 0
-            
-            res = md[0]
-            for i in range(len(md)):
-                if i < len(md)-1:
-                    tmp_next = md[i+1]
-                else:
-                    tmp_next = num
-                
-                if symb[i] == "*":
-                    res*=tmp_next
-                else:
-                    res=int(res/tmp_next)
-            return res
-
-
-
-        for c in s:
-            # print(res)
-            if c.isspace():
-                continue
-            
-            if c == "+" or c == "-":
-                num*=sign
-                sign = 1 if c == "+" else -1
-                if len(md)>0:
-                    res+= md_res(md, symb, num)
-                    # print(md)
-                    # print(res)
-                    md.clear()
-                    symb.clear()
-                else:
-                    res+=num
-                num = 0
-                continue
-            if c == "*" or c=="/":
-                md.append(num*sign)
-                symb.append(c)
-                num = 0
-                sign = 1
-                continue
-            num = num*10 + int(c)
+        num = 0
+        res = 0
+        last_num = 0
+        last_op = '+'
         
-        if len(md)>0:
-            res+= md_res(md, symb, num)
-            # print(md)
-            md.clear()
-            symb.clear()
-            num = 0
-            # print(res)
-        if num != 0:
-            res+=(sign*num)
-        # print(res)
+        for i, c in enumerate(s):
+            if c.isdigit():
+                num = num * 10 + int(c)
+            
+            if c in "+-*/" or i == len(s) - 1:
+                if last_op == '+':
+                    res += last_num
+                    last_num = num
+                elif last_op == '-':
+                    res += last_num
+                    last_num = -num
+                elif last_op == '*':
+                    last_num = last_num * num
+                elif last_op == '/':
+                    last_num = int(last_num / num)
+                
+                last_op = c
+                num = 0
+        
+        res += last_num
         return res
-                
-                
