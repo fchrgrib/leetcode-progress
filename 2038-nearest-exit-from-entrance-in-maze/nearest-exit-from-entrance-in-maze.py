36f257e2c4019bd6c11d73f4c964a79cac526b4d
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-12-18 10:49:46 +0700

    Time: 52 ms (88.03%) | Memory: 19.2 MB (80.91%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
@@ -1,35 +1,26 @@
 class Solution:
-    def nearestExit(self, m: List[List[str]], e: List[int]) -> int:
-        # if e[0] == 0 or e[1] == 0:
-        #     return -1
-        visited = {}
-        step = deque([])
-        step.append(([e[0], e[1]], 0))
-        visited[(e[0], e[1])] = True
-
-        while len(step) > 0:
-            tmp = step.popleft()
-
-            if tmp[0][1]+1<len(m[0]) and (tmp[0][0], tmp[0][1]+1) not in visited and m[tmp[0][0]][tmp[0][1]+1] == ".":
-                if tmp[0][1]+1 == len(m[0])-1 or tmp[0][0] == 0 or tmp[0][0] == len(m)-1:
-                    return tmp[1]+1
-                visited[(tmp[0][0], tmp[0][1]+1)] = True
-                step.append(([tmp[0][0], tmp[0][1]+1], tmp[1]+1))
-            if tmp[0][1]-1>=0 and (tmp[0][0], tmp[0][1]-1) not in visited and m[tmp[0][0]][tmp[0][1]-1] == ".":
-                if tmp[0][1]-1 == 0 or tmp[0][0] == 0 or tmp[0][0] == len(m)-1:
-                    return tmp[1]+1
-                visited[(tmp[0][0], tmp[0][1]-1)] = True
-                step.append(([tmp[0][0], tmp[0][1]-1], tmp[1]+1))
-            if tmp[0][0]+1<len(m) and (tmp[0][0] + 1, tmp[0][1]) not in visited and m[tmp[0][0]+1][tmp[0][1]] == ".":
-                if tmp[0][0]+1 == len(m)-1 or tmp[0][1] == 0 or tmp[0][1] == len(m[0])-1:
-                    return tmp[1]+1
-                visited[(tmp[0][0]+1, tmp[0][1])] = True
-                step.append(([tmp[0][0]+1, tmp[0][1]], tmp[1]+1))
-            if tmp[0][0]-1>=0 and (tmp[0][0] -1, tmp[0][1]) not in visited and m[tmp[0][0]-1][tmp[0][1]] == ".":
-                if tmp[0][0]-1 == 0 or tmp[0][1] == 0 or tmp[0][1] == len(m[0])-1:
-                    return tmp[1]+1
-                visited[(tmp[0][0]-1, tmp[0][1])] = True
-                step.append(([tmp[0][0]-1, tmp[0][1]], tmp[1]+1))
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        rows, cols = len(maze), len(maze[0])
+        start_r, start_c = entrance
+        
+        queue = deque([(start_r, start_c, 0)])
+        maze[start_r][start_c] = '+'
+        
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        while queue:
+            r, c, dist = queue.popleft()
+            
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                
+                if 0 <= nr < rows and 0 <= nc < cols and maze[nr][nc] == ".":
+                    if nr == 0 or nr == rows - 1 or nc == 0 or nc == cols - 1:
+                        return dist + 1
+                    
+                    maze[nr][nc] = '+'
+                    queue.append((nr, nc, dist + 1))
+        
         return -1
 
 
