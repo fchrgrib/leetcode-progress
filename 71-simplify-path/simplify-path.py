86fe124ea73f510e9147fcd7830ad87030a19aef
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-12-19 10:41:12 +0700

    Time: 0 ms (100.00%) | Memory: 17.3 MB (100.00%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,35 +1,15 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        if path[0] != "/":
-            path = "/"+path
         stack = []
-        stack_path = []
+        
+        for part in path.split("/"):
+            if part == "..":
+                if stack:
+                    stack.pop()
+            elif part == "." or part == "":
+                continue
+            else:
+                stack.append(part)
 
-        for i in range(len(path)):
-            if path[i] == "/":
-                if len(stack) != 0:
-                    tmp = stack.pop()
-                    str_m = path[tmp+1:i]
-
-                    if str_m == "..":
-                        if len(stack_path) != 0:
-                            stack_path.pop()
-                        stack.append(i)
-                        continue
-                    elif str_m == "" or str_m == ".":
-                        stack.append(i)
-                        continue
-                    stack_path.append(str_m)
-                stack.append(i)
-        if path[-1] != "/":
-            if len(stack) != 0:
-                    tmp = stack.pop()
-                    str_m = path[tmp+1:]
-
-                    if str_m == "..":
-                        if len(stack_path) != 0:
-                            stack_path.pop()
-                    if not (str_m == "" or str_m == ".") and not str_m == "..":
-                        stack_path.append(str_m)
-        return "/"+"/".join(stack_path)
+        return "/" + "/".join(stack)
         
