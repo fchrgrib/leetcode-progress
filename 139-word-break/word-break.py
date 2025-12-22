3afcf51ecf8a6a341356a557ff378c55a8372292
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-12-22 17:07:08 +0700

    Time: 4 ms (40.00%) | Memory: 17.6 MB (97.39%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {}
+
+        def dfs(start):
+            if start == len(s):
+                return True
+            
+            if start in memo:
+                return memo[start]
+            
+            for word in wordDict:
+                if s.startswith(word, start):
+                    if dfs(start + len(word)):
+                        memo[start] = True
+                        return True
+            
+            memo[start] = False
+            return False
+
+        return dfs(0)
+
+        
