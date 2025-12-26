9c3420d79955b9118089baa018102d649ccf2940
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-12-27 05:13:49 +0700

    Time: 3 ms (68.21%) | Memory: 17.3 MB (96.74%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/remove-duplicate-letters.py
@@ -1,7 +1,7 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         res = []
-        letter_used = []
+        letter_used = set()
         last_letter = {}
 
         for i in range(len(s)):
@@ -15,7 +15,7 @@ class Solution:
             while len(res)>0 and res[-1]>s[i] and i < last_letter[res[-1]]:
                 letter_used.remove(res.pop())
             res.append(s[i])
-            letter_used.append(s[i])
+            letter_used.add(s[i])
 
         
 
