c7536f704d041625f3c5e3ed4ae6d49df2a4ed42
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2025-12-29 05:35:25 +0700

    Time: 59 ms (32.73%) | Memory: 38.5 MB (9.80%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,33 +1,26 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        h = {}
-
-        for i in range(len(nums)):
-            h[nums[i]] = False
-        
-        sum_el = 0
-        sum_max = -2**31
-
-        for k in h.keys():
-            if h[k]:
-                continue
-            h[k] = True
-            
-            i=1
-            while k-i in h:
-                h[k-i] = True
-                sum_el+=1
-                i+=1
-            i=1
-            while k+i in h:
-                h[k+i] = True
-                sum_el+=1
-                i+=1
-            print(sum_el)
-            sum_el+=1
-            sum_max = max(sum_el, sum_max)
-            sum_el = 0
-        if sum_max == -2**31:
+        if len(nums) == 0:
             return 0
-        return sum_max
+        visited = set()
+        max_seq = float("-inf")
+        nums = set(nums)
+        
+        for i in nums:
+            if i in visited:
+                continue
+            curr_seq = 1
+            tmp_i = i
+            visited.add(i)
+            while (tmp_i+1) in nums:
+                tmp_i+=1
+                curr_seq+=1
+                visited.add(tmp_i)
+            tmp_i = i
+            while (tmp_i-1) in nums:
+                tmp_i-=1
+                curr_seq+=1
+                visited.add(tmp_i)
+            max_seq = max(max_seq, curr_seq)
+        return max_seq
         
