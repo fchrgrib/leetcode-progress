fc5ae4a988d1f8fbb3922c666ab050aba93e77ee
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-01-03 15:06:39 +0700

    Time: 952 ms (21.13%) | Memory: 38.3 MB (77.84%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,26 +1,21 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        sum = 0
-        global dp
-        dp = {}
+        dp = [[-1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        lt1 = len(text1)
+        lt2 = len(text2)
 
-        n1 = len(text1)
-        n2 = len(text2)
-        def bc(t1, t2, idx1, idx2, n1, n2):
-            global dp
-            if idx1 == n1 or idx2 == n2:
+        def recurs(i, j):
+            nonlocal text1, text2, lt1, lt2, dp
+            if i>lt1-1 or j>lt2-1:
                 return 0
-            if (idx1, idx2) in dp:
-                return dp[(idx1, idx2)]
+            if dp[i][j] != -1:
+                return dp[i][j]
             
-            dp[(idx1, idx2)] = 0
-            if t1[idx1] == t2[idx2]:
-                print(t1[idx1], t2[idx2])
-                dp[(idx1, idx2)] = max(dp[(idx1, idx2)], 1 + bc(t1, t2, idx1+1, idx2+1, n1, n2))
+            if text1[i]==text2[j]:
+                dp[i][j] = 1 + recurs(i+1, j+1)
             else:
-                dp[(idx1, idx2)] = max(dp[(idx1, idx2)], bc(t1, t2, idx1+1, idx2, n1, n2))
-                dp[(idx1, idx2)] = max(dp[(idx1, idx2)], bc(t1, t2, idx1+1, idx2+1, n1, n2))
-                dp[(idx1, idx2)] = max(dp[(idx1, idx2)], bc(t1, t2, idx1, idx2+1, n1, n2))
-            return dp[(idx1, idx2)]
-        return bc(text1, text2, 0, 0, n1, n2)
+                dp[i][j] = max({recurs(i+1,j), recurs(i,j+1)})
+            return dp[i][j]
+        
+        return recurs(0,0)
         
