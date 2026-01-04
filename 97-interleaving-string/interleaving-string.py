fb32980ad536b6b2c0e64a338500b4fe5968de2b
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-01-04 07:53:37 +0700

    Time: 45 ms (52.08%) | Memory: 19.5 MB (6.31%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,34 +1,31 @@
-class Solution(object):
-    def isInterleave(self, s1, s2, s3):
-        global l1, l2
-        l1 = len(s1)
-        l2 = len(s2)
-        l3 = len(s3)
-        if s1 == s2 == s3 == "":
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        l1, l2, l3 = len(s1), len(s2), len(s3)
+        dp = {}
+        if l1 == l2 == l3 == 0:
             return True
-        if l1 == 0 or l2 == 0:
-            return s1 == s3 or s2 == s3
 
-        if l1 + l2 != l3:
+        if l1+l2 != l3:
             return False
 
-        global memo
-        memo = [[-1 for _ in range(l2+1)] for _ in range(l1+1)]
+        def recurs(i1, i2, i3):
+            nonlocal l1,l2,l3,s1,s2,s3, dp
+            store = ((i1, i2), i3)
 
-        def bc(idx1, idx2,idx3, s1, s2, s3):
-            global memo
-            if idx1 == len(s1) and idx2 == len(s2):
+            if i3 == l3:
                 return True
-            if idx1<=len(s1) and idx2<=len(s2) and memo[idx1][idx2] != -1:
-                return memo[idx1][idx2]
+            if store in dp:
+                return dp[store]
             
-            memo[idx1][idx2] = False
 
-            if idx3< len(s3) and idx1<len(s1) and s3[idx3] == s1[idx1]:
-                memo[idx1][idx2] |= bc(idx1+1, idx2, idx3+1, s1, s2, s3)
-            if idx3< len(s3) and idx2<len(s2) and s3[idx3] == s2[idx2]:
-                memo[idx1][idx2] |= bc(idx1, idx2+1, idx3+1, s1, s2, s3)
+            dp[store] = False
+            next_i1, next_i2 = i1+1, i2+1
 
-            return memo[idx1][idx2]
-
-        return bc(0, 0, 0, s1, s2, s3)
+            if i1 < l1 and s1[i1] == s3[i3]:
+                dp[store] |= recurs(next_i1, i2, i3+1)
+            if i2 < l2 and s2[i2] == s3[i3]:
+                dp[store] |= recurs(i1, next_i2, i3+1)
+            return dp[store]
+        
+        return recurs(0,0,0)
+        
