07f6c5dede5c05d949b6774b7bd588939c81d14e
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-02-05 18:10:01 +0700

    Time: 257 ms (5.03%) | Memory: 48.8 MB (63.01%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -0,0 +1,27 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        heap = []
+        res = 0
+
+        for i in intervals:
+            heapq.heappush(heap, (i[0], i[1]))
+        
+        # print(SortedList(heap))
+        x, y = heapq.heappop(heap)
+
+        while heap:
+            tmp_x, tmp_y = heapq.heappop(heap)
+
+            if tmp_x<y:
+                # print(f"ini tmp: ({tmp_x}, {tmp_y})")
+                # print(f"ini ril: ({x}, {y})")
+
+                dist_ril = abs(y-x)
+                dist_tmp = abs(tmp_y - tmp_x)
+                if y>tmp_y:
+                    x, y = tmp_x, tmp_y
+                res+=1
+            else:
+                y = tmp_y
+                x = tmp_x
+        return res
