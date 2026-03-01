83222d1b336e75dd7145c9725ff92a2dd68fc3d0
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-03-02 06:16:56 +0700

    Time: 18 ms (33.27%) | Memory: 20.5 MB (86.98%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -1,26 +1,18 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        ln = len(nums)
-        if ln == 1:
-            return 0
+        n = len(nums)
+        start, end = -1, -2 
         
-        stack = []
-        start, end = float("inf"), float("-inf")
-
-        for idx, val in enumerate(nums):
-            while stack and nums[stack[-1]]>val:
-                st = stack.pop()
-                start = min(start, st)
-                end = max(end, idx)
-            stack.append(idx)
-        if start == float("inf") or end == float("-inf"):
-            return 0
-        mn = min(nums[start:end+1])
-        mx = max(nums[start:end+1])
-        while end+1<ln and nums[end+1]<mx:
-            end+=1
-        while start > 0 and nums[start-1] > mn:
-            start -= 1
-        return end-start+1
-                
+        max_so_far = nums[0]
+        for i in range(1, n):
+            max_so_far = max(max_so_far, nums[i])
+            if nums[i] < max_so_far:
+                end = i
         
+        min_so_far = nums[-1]
+        for i in range(n-2, -1, -1):
+            min_so_far = min(min_so_far, nums[i])
+            if nums[i] > min_so_far:
+                start = i
+        
+        return end - start + 1
