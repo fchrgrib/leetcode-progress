0ba53979bd50ed6eb40f87f86e6e3773a12d198d
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-03-02 01:55:59 +0700

    Time: 0 ms (100.00%) | Memory: 20.6 MB (85.50%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,23 +1,13 @@
 class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        heap_k, heap_nk = [], []
-        res = []
-
-
-        for num in arr:
-            diff = abs(num-x)
-            if heap_k and -heap_k[0][0]>diff:
-                heapq.heappush(heap_k, (-diff, num))
-            else:
-                heapq.heappush(heap_nk, (diff, num))
-            
-            if heap_k and len(heap_k)>k:
-                df, nm = heapq.heappop(heap_k)
-                heapq.heappush(heap_nk, (-df, nm))
-            elif heap_nk and len(arr) - k < len(heap_nk):
-                df, nm = heapq.heappop(heap_nk)
-                heapq.heappush(heap_k, (-df, nm))
-        while heap_k:
-            res.append(heapq.heappop(heap_k)[1])
-        return sorted(res)
+    def findClosestElements(self, arr, k, x):
+        left, right = 0, len(arr) - k
         
+        while left < right:
+            mid = (left + right) // 2
+            
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return arr[left:left+k]
