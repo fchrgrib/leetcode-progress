9911bc53778b2c55bf26091041a86cfec8447c2f
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-03-06 06:24:22 +0700

    Time: 96 ms (8.04%) | Memory: 22.1 MB (77.83%) - LeetSync

---
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        l = 0
+        prefix_sum = 1
+        res = 0
+
+        for r in range(len(nums)):
+            prefix_sum*=nums[r]
+
+            while l<r and prefix_sum>=k:
+                prefix_sum/=nums[l]
+                l+=1
+            if prefix_sum<k:
+                res+=(r-l+1)
+        return res
+        
