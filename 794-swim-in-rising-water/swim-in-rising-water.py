2f2b19cdfe2a7fabe79a2930e0dd0a4dbfcc4618
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-03-11 13:02:03 +0700

    Time: 32 ms (37.79%) | Memory: 21.3 MB (5.16%) - LeetSync

---
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -0,0 +1,35 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        direction = [(0,1), (1,0), (0,-1), (-1,0)]
+        n = len(grid)
+        l, r = 0, max(max(row) for row in grid)
+        visited = set()
+        def dfs(coor, num):
+            x, y = coor
+            if grid[x][y]>num:
+                return False
+
+            for xx, yy in direction:
+                dx, dy = x+xx, y+yy
+                if dx<0 or dy<0 or dx>=n or dy>=n or (dx, dy) in visited:
+                    continue
+                if grid[dx][dy]>num:
+                    continue
+                if dx == dy == n-1:
+                    return True
+                visited.add((dx, dy))
+                if dfs((dx, dy), num):
+                    return True
+            return False
+        
+        while l<r:
+            m = (l+r)//2
+            if dfs((0,0),m):
+                r = m
+            else:
+                l = m+1
+            visited.clear()
+        return r
+                
+
+        
