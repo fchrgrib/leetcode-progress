c9d6ec989551e8bee480857d1538cda457f1ceb4
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-03-13 13:50:38 +0700

    Time: 80 ms (99.72%) | Memory: 51.5 MB (99.58%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -1,34 +1,25 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        rank = {i:1 for i in range(1,n+1)}
-        parent = {i:i for i in range(1, n+1)}
-        min_dist = {i:float("inf") for i in range(1, n+1)}
+        parent = list(range(n+1))
+        rank = [1]*(n+1)
+        min_dist = [float("inf")]*(n+1)
 
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
 
-        def get_parent(i):
-            par = parent[i]
-            while par != parent[par]:
-                par = parent[par]
-            return par
-        
-        def union_find(node1, node2):
-            par1, par2 = get_parent(node1), get_parent(node2)
+        for a,b,d in roads:
+            pa, pb = find(a), find(b)
 
-            if par1 == par2:
-                return
-            
-            if rank[par1]>rank[par2]:
-                rank[par1]+=rank[par2]
-                parent[par2] = par1
+            if pa != pb:
+                if rank[pa] < rank[pb]:
+                    pa, pb = pb, pa
+                parent[pb] = pa
+                rank[pa] += rank[pb]
+                min_dist[pa] = min(min_dist[pa], min_dist[pb], d)
             else:
-                rank[par2]+=rank[par1]
-                parent[par1] = par2
-        for fr, to, dist in roads:
-            union_find(fr, to)
+                min_dist[pa] = min(min_dist[pa], d)
 
-        for fr, to, dist in roads:
-            par = get_parent(fr)
-            min_dist[par] = min(min_dist[par], dist)
-        return min_dist[get_parent(1)]
-
-        
+        return min_dist[find(1)]
