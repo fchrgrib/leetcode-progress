01957b300ff46d2dffcee787e4fdeca816172b4e
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-03-16 16:12:20 +0700

    Time: 80 ms (28.10%) | Memory: 20.9 MB (56.93%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -3,15 +3,19 @@ class Solution:
         sm = 0
         stack = []
         banned = set(banned)
+        ln = 0
         res = 0
 
         for num in range(1,n+1):
             if num in banned:
                 continue
             sm+=num
+
+            ln+=1
             stack.append(num)
             while sm>maxSum:
                 sm -= stack.pop()
-            res = max(len(stack), res)
+                ln-=1
+            res = max(ln, res)
         return res
         
