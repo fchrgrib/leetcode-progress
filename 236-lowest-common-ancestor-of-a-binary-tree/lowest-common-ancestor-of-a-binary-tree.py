1340682846de6fca575b639795780c81a80ce8e1
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-03-19 14:30:04 +0700

    Time: 68 ms (17.02%) | Memory: 24.4 MB (86.03%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,45 +1,12 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
-    def is_contain(self, root: 'TreeNode', val:int):
-        if not root:
-            return False
-        
-        if root.val == val:
-            return True
-        lft = self.is_contain(root.left, val)
-        if not lft:
-            return self.is_contain(root.right, val)
-        return lft
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return None
-        p_val, q_val, curr_val = p.val, q.val, root.val
-
-
-        if p_val == curr_val or q_val == curr_val:
+        if not root or root == p or root == q:
             return root
-
-        if not root.right:
-            return self.lowestCommonAncestor(root.left, p, q)
         
-        if not root.left:
-            return self.lowestCommonAncestor(root.right, p, q)
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
-        find_lft, find_right = self.is_contain(root.left, p_val), self.is_contain(root.right, q_val)
-        if not find_lft and not find_right:
-            find_lft, find_right = self.is_contain(root.left, q_val), self.is_contain(root.right, p_val)
-
-        if find_lft and find_right:
+        if left and right:
             return root
-        elif find_lft:
-            return self.lowestCommonAncestor(root.left, p, q)
-        else:
-            return self.lowestCommonAncestor(root.right, p, q)
-
         
+        return left if left else right
