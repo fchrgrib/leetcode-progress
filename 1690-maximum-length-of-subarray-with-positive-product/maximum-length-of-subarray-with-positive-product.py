8ecc5cbeb9216d8c3595e0bb9b156f5c2a3f434a
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-03-24 21:11:49 +0700

    Time: 35 ms (70.97%) | Memory: 33.7 MB (37.74%) - LeetSync

--- 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.py
+++ 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.py
@@ -1,36 +1,18 @@
 class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
+        pos = 0
+        neg = 0
         res = 0
-        ln = len(nums)
-        l = 0
-        pos_neg = [0,0]
 
-        def get_long(i):
-            if i>=ln:
-                return 0
-            while pos_neg[0]%2 == 1:
-                if nums[i]>0:
-                    pos_neg[1]-=1
-                else:
-                    pos_neg[0]-=1
-                i+=1
-            return pos_neg[1] + pos_neg[0]
-
-        for r in range(ln):
-            if nums[r] == 0:
-                res = max(res, get_long(l))
-                pos_neg = [0,0]
-                l = r+1
-                continue
-            
-            if nums[r]>0:
-                pos_neg[1]+=1
+        for num in nums:
+            if num == 0:
+                pos = neg = 0
+            elif num > 0:
+                pos += 1
+                neg = neg + 1 if neg > 0 else 0
             else:
-                pos_neg[0]+=1
+                pos, neg = (neg + 1 if neg > 0 else 0), pos + 1
             
-            if pos_neg[0]%2==0:
-                res = max(res, pos_neg[0]+pos_neg[1])
-        return max(res, get_long(l))
-                
-                
+            res = max(res, pos)
         
+        return res
