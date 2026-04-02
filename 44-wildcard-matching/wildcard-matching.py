4581d48e5d8da9c9444685e9d4a01d8aa498adcf
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-04-02 16:48:22 +0700

    Time: 1369 ms (5.02%) | Memory: 119.4 MB (9.85%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        ln1, ln2 = len(s), len(p)
+        if ln1 == ln2 == 0:
+            return True
+        if ln1 == 0:
+            chrc = list(set(list(p)))
+            return len(chrc) == 1 and chrc[0] == "*"
+
+        dp = {}
+
+        def backtrack(idx1, idx2):
+            store = (idx1, idx2)
+            if store in dp:
+                return dp[store]
+            if idx1 == ln1 and idx2 == ln2:
+                return True
+            if idx2 >= ln2:
+                return False
+            if idx1<ln1 and s[idx1] != p[idx2] and p[idx2] != "*" and p[idx2] != "?":
+                return False
+
+            dp[store] = False
+            if idx1<ln1 and (s[idx1] == p[idx2] or p[idx2] == "?"):
+                dp[store] = backtrack(idx1+1, idx2+1)
+                return dp[store]
+            if p[idx2] != "*":
+                return False
+            for i in range(idx1, ln1+1):
+                while idx2<ln2 and p[idx2] == "*":
+                    idx2+=1
+                dp[store] |= backtrack(i,idx2)
+            return dp[store]
+
+
+        return backtrack(0,0)
+        
