9c2a5b87e295b189886fbd4863f1313cd3fa6359
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-04-02 16:53:48 +0700

    Time: 721 ms (33.18%) | Memory: 50.6 MB (47.64%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,38 +1,19 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        ln1, ln2 = len(s), len(p)
-        if ln1 == ln2 == 0:
-            return True
-        if ln1 == 0:
-            chrc = list(set(list(p)))
-            return len(chrc) == 1 and chrc[0] == "*"
-
-        dp = {}
-
-        def backtrack(idx1, idx2):
-            store = (idx1, idx2)
-            if store in dp:
-                return dp[store]
-            if idx1 == ln1 and idx2 == ln2:
-                return True
-            if idx2 == ln2-1 and p[idx2] == "*":
-                return True
-            if idx2 >= ln2:
-                return False
-            if idx1 < ln1 and s[idx1] != p[idx2] and p[idx2] != "*" and p[idx2] != "?":
-                return False
-
-            dp[store] = False
-            if idx1<ln1 and (s[idx1] == p[idx2] or p[idx2] == "?"):
-                dp[store] = backtrack(idx1+1, idx2+1)
-                return dp[store]
-            
-            for i in range(idx1, ln1+1):
-                while idx2<ln2 and p[idx2] == "*":
-                    idx2+=1
-                dp[store] |= backtrack(i,idx2)
-            return dp[store]
-
-
-        return backtrack(0,0)
+        n, m = len(s), len(p)
         
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
+        dp[0][0] = True
+        
+        for j in range(m):
+            if p[j] == "*":
+                dp[0][j+1] = dp[0][j]
+        
+        for i in range(n):
+            for j in range(m):
+                if p[j] == s[i] or p[j] == "?":
+                    dp[i+1][j+1] = dp[i][j]
+                elif p[j] == "*":
+                    dp[i+1][j+1] = dp[i][j+1] or dp[i+1][j]
+        
+        return dp[n][m]
