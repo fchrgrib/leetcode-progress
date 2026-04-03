2fbd8b2aba14b7363abfc7b11736d75c5d8b1b94
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-04-03 22:52:04 +0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (35.90%) - LeetSync

--- 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
+++ 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
@@ -6,9 +6,7 @@ class Solution:
             while idx<len(get_str) and get_str[idx] == "0":
                 idx+=1
             if idx!=0:
-                tmp = get_str[idx]
-                get_str[idx] = get_str[0]
-                get_str[0] = tmp
+                get_str[0], get_str[idx] = get_str[idx], get_str[0]
             return int("".join(get_str))
         elif num<0:
             get_str = sorted(list(str(num)[1:]), reverse=True)
