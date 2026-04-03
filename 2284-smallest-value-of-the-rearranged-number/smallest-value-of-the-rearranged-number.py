70e48d56a150950a1518bf5e671b24704a18b0e5
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-04-03 22:50:45 +0700

    Time: 2 ms (22.79%) | Memory: 19.2 MB (93.45%) - LeetSync

---
+++ 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def smallestNumber(self, num: int) -> int:
+        if num>0:
+            get_str = sorted(list(str(num)))
+            idx = 0
+            while idx<len(get_str) and get_str[idx] == "0":
+                idx+=1
+            if idx!=0:
+                tmp = get_str[idx]
+                get_str[idx] = get_str[0]
+                get_str[0] = tmp
+            return int("".join(get_str))
+        elif num<0:
+            get_str = sorted(list(str(num)[1:]), reverse=True)
+            return -1*int("".join(get_str))
+        return 0
+        
