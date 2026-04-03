16a27d2efc3d68f3142b9aa7e14fab26bf6d236d
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2026-04-03 22:37:29 +0700

    Time: 3 ms (60.65%) | Memory: 19.3 MB (44.65%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,23 +1,16 @@
 class Solution:
-    def largestNumber(self, nums: List[int]) -> str:
-        str_list = list(map(str, nums))
-        dct_list = defaultdict(list)
-        res = ""
+    def largestNumber(self, nums):
+        strs = list(map(str, nums))
+
         def compare(x, y):
             if x + y > y + x:
-                return -1   
+                return -1
             elif x + y < y + x:
-                return 1   
-            else:
-                return 0
-        for c in str_list:
-            dct_list[c[0]].append(c)
-        
-        for i in range(9,-1,-1):
-            st = str(i)
-            if st in dct_list:
-                dct_list[st].sort(key=cmp_to_key(compare))
-                res+="".join(dct_list[st])
-        
-        return str(int(res))
-        
+                return 1
+            return 0
+
+        strs.sort(key=cmp_to_key(compare))
+
+        result = "".join(strs)
+
+        return "0" if result[0] == "0" else result
